ad5ac262ab1f2c8ee648716bea15d58f98714697
Ashwin Prakash <ashp94@gmail.com>
2018-11-30 11:09:56 -0600

    actual final version

--- chicken.js
+++ chicken.js
@@ -1,17 +1,13 @@
 for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
-
-    if (currentNumber % 5 === 0) {
-        console.log("Chicken") 
-        } else {
-        console.log(currentNumber)
-        }
-    
-    if (currentNumber % 7 === 0) {
-        console.log("Monkey")
-        } else {
-        console.log(currentNumber)
-        }
     if (currentNumber % 5 ===0 && currentNumber % 7 ===0){
-        console.log("ChickenMonkey")
-        } 
+        console.log("ChickenMonkey") 
+    }
+        else if (currentNumber % 7 === 0) {
+        console.log("Monkey")
+        }
+        else if (currentNumber % 5 === 0) {
+            console.log("Chicken")  
+        } else {
+        console.log(currentNumber)
+        }
     }
